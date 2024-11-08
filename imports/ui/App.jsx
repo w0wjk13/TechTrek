f90afb78a858258e0bac58a0291e32ef21c577a9
w0wjk13 <w0wjk13@naver.com>
2024-11-08 15:35:50 +0900

    testpage commit

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,12 +4,14 @@ import Home from "./Home.jsx";
 import NotFound from "./NotFound.jsx";
 import Nav from "./Nav.jsx";
 import PostDetail from "./PostDetail.jsx";
+import TestPage from './TestPage.jsx';
 
 export const App = () => (
   <Router>
     <Nav />
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/testpage" element={<TestPage />}></Route>
       <Route path="/postDetail/:_id" element={<PostDetail />}></Route>
       <Route path="*" element={<NotFound />}></Route>
     </Routes>
