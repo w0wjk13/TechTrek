58acce57a3af729035527048f26e7ab0ad42e1ab
Danbi <verisign_90@naver.com>
2024-11-14 21:23:56 +0900

    요구하는 역량과 점수

--- imports/ui/uploadStudy/UploadStudy.jsx
+++ imports/ui/uploadStudy/UploadStudy.jsx
@@ -28,17 +28,26 @@ const UploadStudy = () => {
     "ReactNative",
   ];
 
-  const onOfflineRef = useRef(null);
-  const numberPeopleRef = useRef(null);
+  const numberPeopleRef = useRef(null); //모집인원
   const [stackList, setStackList] = useState([]); //선택한 기술스택 목록
-  const [date, setDate] = useState(null);
+  const [date, setDate] = useState(null); //모집마감일
+  const [studyType, setStudyType] = useState("offline"); //오프라인일 경우 주소입력창 보여주기
+  const addressRef = useRef(null); //주소
+  const formRef = useRef(null); //백엔드프론트 온라인오프라인
+  const titleRef = useRef(null);
+  const contentRef = useRef(null);
+  const [score, setScore] = useState({}); //요구하는 역량과 점수
 
-  const addressMap = () => {};
+  const gifts = ["manner", "mentoring", "passion", "communication", "time"]; //역량 종류
 
   const addStack = (e) => {
     const selectStack = e.target.value;
-    //사용자가 스택을 선택했고 && useState([])에 사용자가 선택한 스택이 없다면 추가
-    if (selectStack && !stackList.includes(selectStack)) {
+    //사용자가 스택을 선택했고 && useState([])에 사용자가 선택한 스택이 없다면 추가(최대 5개 스택)
+    if (
+      selectStack &&
+      !stackList.includes(selectStack) &&
+      stackList.length < 5
+    ) {
       setStackList([...stackList, selectStack]);
     }
   };
@@ -50,11 +59,21 @@ const UploadStudy = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    const formData = new FormData(formRef.current);
+
     const uploadData = {
-      onOffline: onOfflineRef.current.value,
-      numberPeopleRef: numberPeopleRef.current.value,
-      stackList: stackList,
-      date: date,
+      roles: formData.get("studyType"), //모집분야(프론트/백)
+      onOffline: studyType, //모집형태(온/오프라인)
+      //주소
+      address:
+        formData.get("studyType") === "offline"
+          ? addressRef.current.value
+          : null,
+      studyCount: numberPeopleRef.current.value, //모집인원
+      techStack: stackList, //기술스택
+      studyClose: date,
+      title: titleRef.current.value,
+      content: contentRef.current.value,
     };
 
     Meteor.call("uploadStudy", uploadData, (err) => {
@@ -69,15 +88,66 @@ const UploadStudy = () => {
   return (
     <>
       <h2>스터디 생성 페이지</h2>
-      <form onSubmit={handleSubmit}>
-        <select ref={onOfflineRef} defaultValue="offline">
-          <option value="online">온라인</option>
-          <option value="offline">오프라인</option>
-        </select>
-        <br />
+      <form onSubmit={handleSubmit} ref={formRef}>
+        <h3>모집 분야</h3>
+        <div>
+          <div>
+            <input
+              type="radio"
+              id="all"
+              name="roles"
+              value="전체"
+              defaultChecked
+            />
+            <label htmlFor="all">전체</label>
+          </div>
+          <div>
+            <input type="radio" id="frontend" name="roles" value="프론트엔드" />
+            <label htmlFor="frontend">프론트엔드</label>
+          </div>
+          <div>
+            <input type="radio" id="backend" name="roles" value="백엔드" />
+            <label htmlFor="backend">백엔드</label>
+          </div>
+        </div>
 
-        <select ref={numberPeopleRef} defaultValue="1">
-          {Array.from({ length: 10 }, (_, i) => (
+        <h3>모임 형태</h3>
+        <div>
+          <div>
+            <input
+              type="radio"
+              id="online"
+              name="studyType"
+              value="online"
+              onChange={(e) => setStudyType(e.target.value)}
+            />
+            <label htmlFor="online">온라인</label>
+          </div>
+
+          <div>
+            <input
+              type="radio"
+              id="offline"
+              name="studyType"
+              value="offline"
+              onChange={(e) => setStudyType(e.target.value)}
+            />
+            <label htmlFor="offline">오프라인</label>
+          </div>
+
+          {studyType === "offline" && (
+            <>
+              <input type="text" placeholder="주소 입력하기" ref={addressRef} />
+            </>
+          )}
+          <br />
+        </div>
+
+        <select ref={numberPeopleRef} defaultValue="모집인원">
+          <option value="모집인원" disabled>
+            모집인원
+          </option>
+          {Array.from({ length: 9 }, (_, i) => (
             <option key={i + 1} value={i + 1}>
               {i + 1}명
             </option>
@@ -85,10 +155,14 @@ const UploadStudy = () => {
         </select>
         <br />
 
-        <div className="stackContainer">
-          <select onChange={addStack} defaultValue="">
+        <div>
+          <select
+            onChange={addStack}
+            defaultValue=""
+            disabled={stackList.length >= 5}
+          >
             <option value="" disabled>
-              기술스택
+              기술스택 (최대 5개)
             </option>
             {stackOptions.map((stack, index) => (
               <option key={index} value={stack}>
@@ -98,16 +172,15 @@ const UploadStudy = () => {
           </select>
           <br />
 
-          <div className="selectStackList">
-            {stackList.map((stack, index) => (
-              <span key={index} className="tag">
-                {stack}
-                <button type="button" onClick={() => removeStack(stack)}>
-                  X
-                </button>
-              </span>
-            ))}
-          </div>
+          {stackList.map((stack, index) => (
+            <span key={index}>
+              {stack}
+              <button type="button" onClick={() => removeStack(stack)}>
+                X
+              </button>
+            </span>
+          ))}
+          <br />
         </div>
 
         <DatePicker
@@ -119,50 +192,22 @@ const UploadStudy = () => {
         />
         <br />
 
-        <a href="#" onClick={addressMap}>
-          주소 선택
-        </a>
-        <br />
+        <div>
+          {gifts.map((gift) => (
+            <div key={}></div>
+          ))}
+        </div>
 
+        <div>
+          <input type="text" ref={titleRef} placeholder="제목을 입력하세요" />
+          <br />
+          <textarea
+            style={{ width: "500px", height: "100px" }}
+            ref={contentRef}
+            placeholder="프로젝트에 대해 설명해 주세요"
+          />
+        </div>
         <button type="submit">제출하기</button>
-
-        <style jsx>{`
-          .stackContainer {
-            display: flex;
-            align-items: center;
-            border: 1px solid #ccc;
-            padding: 5px;
-            border-radius: 5px;
-            justify-content: flex-start;
-          }
-
-          .selectStackList {
-            display: flex;
-            flex-wrap: wrap;
-            margin-left: 10px;
-            justify-content: flex-start;
-          }
-
-          .tag {
-            background-color: #e0e0e0;
-            padding: 5px;
-            margin-right: 5px;
-            border-radius: 3px;
-            display: flex;
-            align-items: center;
-          }
-
-          .tag button {
-            margin-left: 5px;
-            background-color: transparent;
-            border: none;
-            cursor: pointer;
-          }
-
-          select {
-            margin-left: 0;
-          }
-        `}</style>
       </form>
     </>
   );
